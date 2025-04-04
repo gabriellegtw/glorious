26508569bdf41f45acb5a1a94b37132bf1ffedea
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2025-04-04 11:00:50 +0800

    Move env file

--- glorious/src/api/server.js
+++ glorious/src/api/server.js
@@ -9,7 +9,7 @@ import { dirname, join } from 'path';
 // Get the correct path to .env file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-dotenv.config({ path: join(__dirname, '../../.env') });
+dotenv.config({ path: join(__dirname, '../../../.env') });
 
 const app = express();
 
